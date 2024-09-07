2ad66c95000526dee41fe2b9c000a56465081851
darshakkanani2002 <kdarshak2910@gmail.com>
2024-09-07 14:01:40 +0530

    post load options

--- src/component/pages/Post.jsx
+++ src/component/pages/Post.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 import Select from 'react-select';
+import { Test_Api } from '../Config';
 
 export default function Post() {
+    const [category, setCategory] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [post, setPost] = useState([]);
+    const [postData, setPostData] = useState({
+        vCatId: '',
+        vImage: '',
+        vStartColor: '',
+        vEndColor: '',
+        vTextColor: '',
+        vLanguageCode: '',
+        vLanguageId: ''
+    })
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        loadOptions();
+    }, [])
+
+    const loadOptions = async () => {
+        try {
+            const response = await axios.post(`${Test_Api}category/list`, { vLanguageId: '65f1272c2a844399c0486c75' });
+            const data = response.data.data.map(category => ({
+                label: category.vName,
+                value: category._id,
+                id: category._id
+            }));
+            setOptions(data);
+        } catch (error) {
+            console.error('Error fetching options:', error);
+        }
+    };
+
+    // category select handle ---------------------------
+    const handleCategorySelect = (selectedOption) => {
+        setSelectedCategory(selectedOption);
+        setPostData(prevState => ({
+            ...prevState,
+            vCatId: selectedOption ? selectedOption.id : ''
+        }));
+        if (selectedOption) {
+            fetchData(selectedOption.id);
+            console.log("Selected Options ===>", selectedOption);
+
+        }
+    };
     return (
         <div>
             <div className='my-3'>
@@ -10,7 +57,13 @@ export default function Post() {
                         <div className='row'>
                             <div className='col-lg-12 mb-3'>
                                 <label htmlFor="">Category Name</label>
-                                <Select></Select>
+                                <Select className='mb-3'
+                                    value={selectedCategory}
+                                    onChange={handleCategorySelect}
+                                    onMenuOpen={loadOptions}
+                                    options={options}
+                                    required
+                                />
                             </div>
                             <div className='col-lg-6 mb-3'>
                                 <label htmlFor="image">Image</label>
