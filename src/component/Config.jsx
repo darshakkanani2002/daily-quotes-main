d163b70d28308d8ca581c210e0d84f3adf875519
darshakkanani2002 <kdarshak2910@gmail.com>
2024-09-24 18:07:02 +0530

    changes in config live api url

--- src/component/Config.jsx
+++ src/component/Config.jsx
@@ -1,6 +1,6 @@
 // Test Api -------------------------------------------------------------------
-export const Test_Api = 'http://143.244.139.153:4500/api/v1/';
-export const Img_Url = 'http://143.244.139.153:4500/';
+export const Test_Api = 'http://192.168.1.3:4500/api/v1/';
+export const Img_Url = 'http://192.168.1.3:4500/';
 
 // Live Api -------------------------------------------------------------------
 export const Live_Api = 'http://143.244.139.153:4500/api/v1/';
